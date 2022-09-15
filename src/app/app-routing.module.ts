377fbc6db14ec1297f066a4a37cd4f60d6274bf5
Rakesh Reddy <rakesh.reddy@argano.com>
2022-09-15 17:26:57 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ChildrenOutletContexts, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AboutusComponent } from './main/aboutus/aboutus.component';
+import { ContactusComponent } from './main/contactus/contactus.component';
 import { HeaderComponent } from './main/header/header.component';
 import { MainComponent } from './main/main/main.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'main', component: MainComponent, children:[
-    {path: 'header', component: HeaderComponent}
+    {path: 'header', component: HeaderComponent},
+    {path: 'aboutus', component: AboutusComponent},
+    {path: 'contactus', component: ContactusComponent},
   ]},
 
 
